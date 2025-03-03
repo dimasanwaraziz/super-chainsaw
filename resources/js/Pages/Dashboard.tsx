52ead2242dff95d01f4fe1fda5eac1ae74e89b1c
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2025-03-03 12:33:04 +0700

    Add charts and update layout in Dashboard and Pengajuan pages

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,17 +1,236 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from '@/components/ui/chart';
 import DashboardLayout from '@/Layouts/DashboardLayout';
 import { Head } from '@inertiajs/react';
+import { TrendingUp } from 'lucide-react';
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Label,
+    Pie,
+    PieChart,
+    XAxis,
+    YAxis
+} from 'recharts';
+
+const chartData = [
+    { month: 'January', desktop: 186 },
+    { month: 'February', desktop: 305 },
+    { month: 'March', desktop: 237 },
+    { month: 'April', desktop: 73 },
+    { month: 'May', desktop: 209 },
+    { month: 'June', desktop: 214 },
+];
+
+const chartConfig = {
+    desktop: {
+        label: 'Desktop',
+        color: 'hsl(var(--chart-1))',
+    },
+    mobile: {
+        label: 'Mobile',
+        color: 'hsl(var(--chart-2))',
+    },
+} satisfies ChartConfig;
 
 export default function Dashboard() {
     return (
         <DashboardLayout>
-            <Head title="Dashboard" />
+            <Head title="Loket Pengajuan" />
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="aspect-video rounded-xl bg-muted/50" />
-                    <div className="aspect-video rounded-xl bg-muted/50" />
-                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Bar Chart</CardTitle>
+                            <CardDescription>
+                                January - June 2024
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <ChartContainer config={chartConfig}>
+                                <BarChart accessibilityLayer data={chartData}>
+                                    <CartesianGrid vertical={false} />
+                                    <XAxis
+                                        dataKey="month"
+                                        tickLine={false}
+                                        tickMargin={10}
+                                        axisLine={false}
+                                        tickFormatter={(value) =>
+                                            value.slice(0, 3)
+                                        }
+                                    />
+                                    <ChartTooltip
+                                        cursor={false}
+                                        content={
+                                            <ChartTooltipContent hideLabel />
+                                        }
+                                    />
+                                    <Bar
+                                        dataKey="desktop"
+                                        fill="var(--color-desktop)"
+                                        radius={8}
+                                    />
+                                </BarChart>
+                            </ChartContainer>
+                        </CardContent>
+                        <CardFooter className="flex-col items-start gap-2 text-sm">
+                            <div className="flex gap-2 font-medium leading-none">
+                                Trending up by 5.2% this month{' '}
+                                <TrendingUp className="h-4 w-4" />
+                            </div>
+                            <div className="leading-none text-muted-foreground">
+                                Showing total visitors for the last 6 months
+                            </div>
+                        </CardFooter>
+                    </Card>
+                    <Card className="flex flex-col">
+                        <CardHeader className="items-center pb-0">
+                            <CardTitle>Pie Chart - Donut with Text</CardTitle>
+                            <CardDescription>
+                                January - June 2024
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="flex-1 pb-0">
+                            <ChartContainer
+                                config={chartConfig}
+                                className="mx-auto aspect-square max-h-[250px]"
+                            >
+                                <PieChart>
+                                    <ChartTooltip
+                                        cursor={false}
+                                        content={
+                                            <ChartTooltipContent hideLabel />
+                                        }
+                                    />
+                                    <Pie
+                                        data={chartData}
+                                        dataKey="visitors"
+                                        nameKey="browser"
+                                        innerRadius={60}
+                                        strokeWidth={5}
+                                    >
+                                        <Label
+                                            content={({ viewBox }) => {
+                                                if (
+                                                    viewBox &&
+                                                    'cx' in viewBox &&
+                                                    'cy' in viewBox
+                                                ) {
+                                                    return (
+                                                        <text
+                                                            x={viewBox.cx}
+                                                            y={viewBox.cy}
+                                                            textAnchor="middle"
+                                                            dominantBaseline="middle"
+                                                        >
+                                                            <tspan
+                                                                x={viewBox.cx}
+                                                                y={viewBox.cy}
+                                                                className="fill-foreground text-3xl font-bold"
+                                                            >
+                                                                {totalVisitors.toLocaleString()}
+                                                            </tspan>
+                                                            <tspan
+                                                                x={viewBox.cx}
+                                                                y={
+                                                                    (viewBox.cy ||
+                                                                        0) + 24
+                                                                }
+                                                                className="fill-muted-foreground"
+                                                            >
+                                                                Visitors
+                                                            </tspan>
+                                                        </text>
+                                                    );
+                                                }
+                                            }}
+                                        />
+                                    </Pie>
+                                </PieChart>
+                            </ChartContainer>
+                        </CardContent>
+                        <CardFooter className="flex-col gap-2 text-sm">
+                            <div className="flex items-center gap-2 font-medium leading-none">
+                                Trending up by 5.2% this month{' '}
+                                <TrendingUp className="h-4 w-4" />
+                            </div>
+                            <div className="leading-none text-muted-foreground">
+                                Showing total visitors for the last 6 months
+                            </div>
+                        </CardFooter>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Bar Chart - Mixed</CardTitle>
+                            <CardDescription>
+                                January - June 2024
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <ChartContainer config={chartConfig}>
+                                <BarChart
+                                    accessibilityLayer
+                                    data={chartData}
+                                    layout="vertical"
+                                    margin={{
+                                        left: 0,
+                                    }}
+                                >
+                                    <YAxis
+                                        dataKey="browser"
+                                        type="category"
+                                        tickLine={false}
+                                        tickMargin={10}
+                                        axisLine={false}
+                                        tickFormatter={(value) =>
+                                            chartConfig[
+                                                value as keyof typeof chartConfig
+                                            ]?.label
+                                        }
+                                    />
+                                    <XAxis
+                                        dataKey="visitors"
+                                        type="number"
+                                        hide
+                                    />
+                                    <ChartTooltip
+                                        cursor={false}
+                                        content={
+                                            <ChartTooltipContent hideLabel />
+                                        }
+                                    />
+                                    <Bar
+                                        dataKey="visitors"
+                                        layout="vertical"
+                                        radius={5}
+                                    />
+                                </BarChart>
+                            </ChartContainer>
+                        </CardContent>
+                        <CardFooter className="flex-col items-start gap-2 text-sm">
+                            <div className="flex gap-2 font-medium leading-none">
+                                Trending up by 5.2% this month{' '}
+                                <TrendingUp className="h-4 w-4" />
+                            </div>
+                            <div className="leading-none text-muted-foreground">
+                                Showing total visitors for the last 6 months
+                            </div>
+                        </CardFooter>
+                    </Card>
                 </div>
-                <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
             </div>
         </DashboardLayout>
     );
