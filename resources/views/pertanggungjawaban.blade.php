e594f29311e71059332652f8113c030d95ecfd6b
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2025-03-04 14:39:33 +0700

    Add side-by-side tables to Pertanggungjawaban view for improved data presentation

--- resources/views/pertanggungjawaban.blade.php
+++ resources/views/pertanggungjawaban.blade.php
@@ -23,5 +23,47 @@
     <div id="watermark">
         <img src="{{ public_path('images/watermark-setjen-dpr-ri.png') }}" alt="Watermark" style="width: 500px; opacity: 0.6;">
     </div>
+    <div style="display: flex; justify-content: space-between; flex-direction: row;">
+        <div style="width: 48%;">
+            <table border="1" cellpadding="5" cellspacing="0" style="width: 100%;">
+                <thead>
+                    <tr>
+                        <th>Left Header 1</th>
+                        <th>Left Header 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Left Data 1</td>
+                        <td>Left Data 2</td>
+                    </tr>
+                    <tr>
+                        <td>Left Data 3</td>
+                        <td>Left Data 4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div style="width: 48%;">
+            <table border="1" cellpadding="5" cellspacing="0" style="width: 100%;">
+                <thead>
+                    <tr>
+                        <th>Right Header 1</th>
+                        <th>Right Header 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Right Data 1</td>
+                        <td>Right Data 2</td>
+                    </tr>
+                    <tr>
+                        <td>Right Data 3</td>
+                        <td>Right Data 4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
