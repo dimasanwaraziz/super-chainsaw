622236c0edd1bca3281fd47d39ab23861e733c19
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2025-03-05 14:36:30 +0700

    Enhance Pertanggungjawaban view with improved layout and additional details for travel reports

--- resources/views/pertanggungjawaban.blade.php
+++ resources/views/pertanggungjawaban.blade.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <style>
         body {
             font-family: Arial, sans-serif;
+            font-size: 8px;
         }
+
         #watermark {
             position: fixed;
             top: 13%;
@@ -19,51 +22,208 @@
     <meta charset="UTF-8">
     <title>Users Report</title>
 </head>
+
 <body>
     <div id="watermark">
-        <img src="{{ public_path('images/watermark-setjen-dpr-ri.png') }}" alt="Watermark" style="width: 500px; opacity: 0.6;">
+        <img src="{{ public_path('images/watermark-setjen-dpr-ri.png') }}" alt="Watermark"
+            style="width: 500px; opacity: 0.6;">
     </div>
-    <div style="display: flex; justify-content: space-between; flex-direction: row;">
-        <div style="width: 48%;">
-            <table border="1" cellpadding="5" cellspacing="0" style="width: 100%;">
-                <thead>
-                    <tr>
-                        <th>Left Header 1</th>
-                        <th>Left Header 2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Left Data 1</td>
-                        <td>Left Data 2</td>
-                    </tr>
-                    <tr>
-                        <td>Left Data 3</td>
-                        <td>Left Data 4</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div style="width: 48%;">
-            <table border="1" cellpadding="5" cellspacing="0" style="width: 100%;">
-                <thead>
-                    <tr>
-                        <th>Right Header 1</th>
-                        <th>Right Header 2</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Right Data 1</td>
-                        <td>Right Data 2</td>
-                    </tr>
-                    <tr>
-                        <td>Right Data 3</td>
-                        <td>Right Data 4</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <table border="1" cellspacing="0" cellpadding="5" style="width: 100%; border-collapse: collapse;">
+        <thead>
+            <tr>
+                <td style="width: 50%;" colspan="4">
+                    <table style="width: 100%; border: none;">
+                        <tr>
+                            <td style="width: 50%; border: none;">
+                                <p style="text-align: center; font-weight: bold;">DEWAN PERWAKILAN RAKYAT<br>REPUBLIK
+                                    INDONESIA<br><u>JAKARTA</u></p>
+                            </td>
+                            <td style="width: 50%; border: none;">
+                                <p style="text-align: right; font-size: 6px;">Lampiran I</p>
+                                <p style="text-align: justify; font-size: 6px;">PERATURAN MENTERI KEUANGAN NOMOR 113 /
+                                    PMK. 05 / 2012 TENTANG PERJALANAN DINAS DALAM
+                                    NEGERI BAGI PEJABAT NEGARA, PEGAWAI NEGERI, DAN PEGAWAI TIDAK TETAP</p>
+                                <table>
+                                    <tr>
+                                        <td style="width: 50%; border: none;">
+                                            <div style="text-align: left;">Lembar ke</div>
+                                            <div style="text-align: left;">Kode No</div>
+                                            <div style="text-align: left;">Nomor</div>
+                                        </td>
+                                        <td style="width: 50%; border: none;">
+                                            <div style="text-align: left;">: 1</div>
+                                            <div style="text-align: left;">: D / 2025</div>
+                                            <div style="text-align: left;">: 140 / DEWAN / PERJADIN / I / 2025</div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <p style="text-align: center; font-weight: bold; text-decoration: underline;">SURAT PERJALANAN DINAS
+                        (SPD)</p>
+                </td>
+                <td style="width: 1%;"></td>
+                <td style="width: 25%; text-align: left; vertical-align: top;">I</td>
+                <td style="width: 25%;">
+                    <table style="width: 100%; border: none;">
+                        <tr>
+                            <td style="width: 20%; border: none;">Berangkat dari</td>
+                            <td style="width: 80%; border: none;">: Jakarta</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; border: none;">pada tanggal</td>
+                            <td style="width: 50%; border: none;">: 22 Januari 2025</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; border: none;">ke</td>
+                            <td style="width: 50%; border: none;">: Tangerang</td>
+                        </tr>
+                    </table>
+                    <br>
+                    <div width="100%" style="text-align: center;">
+                        <p style="width: 60%; margin: 0 auto;">PEJABAT PEMBUAT KOMITMEN BIDANG PERSIDANGAN I</p>
+                        <br><br><br>
+                        <p><u><b>EFFIE, S.SOS., M.A.P.</b></u><br>NP. 19680215 199003 2 003</p>
+                    </div>
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="width: 2%">1</td>
+                <td colspan="2">Pejabat Pembuat komitmen</td>
+                <td>BIDANG PERSIDANGAN I</td>
+                <td></td>
+                <td rowspan="3" style="vertical-align: top;">
+                    <div style="float: left; width: 10%;">II</div>
+                    <div style="float: left; width: 90%;">
+                        <table>
+                            <tr>
+                                <td style="width: 50%; border: none;">
+                                    <div style="text-align: left;">Tiba di</div>
+                                    <div style="text-align: left;">pada tanggal</div>
+                                    <div style="text-align: left;">Kepala</div>
+                                </td>
+                                <td style="width: 50%; border: none;">
+                                    <div style="text-align: left;">: Tangerang</div>
+                                    <div style="text-align: left;">: 22 Januari 2025</div>
+                                    <div style="text-align: left;">: </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </td>
+                <td rowspan="3" style="vertical-align: top;">
+                    <table style="width: 100%; border: none;">
+                        <tr>
+                            <td style="width: 20%; border: none;">Berangkat dari</td>
+                            <td style="width: 80%; border: none;">: Jakarta</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; border: none;">pada tanggal</td>
+                            <td style="width: 50%; border: none;">: 22 Januari 2025</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; border: none;">ke</td>
+                            <td style="width: 50%; border: none;">: Tangerang</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td colspan="2">Nama/NIP yang melaksanakan perjalanan dinas</td>
+                <td>SITI HEDIATI SOEHARTO, S.E.</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td colspan="2">a. Pangkat dan Golongan Ruang Gaji menurut PP No. 6 Tahun 1997<br>b. Jabatan /
+                    Instansi<br>c. Tingkat Biaya Perjalanan Dinas</td>
+                <td>...<br>Anggota DPR RI NO A-117<br>A</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td colspan="2">Maksud perjalanan dinas</td>
+                <td>Kunjungan Kerja Spesifik Komisi IV DPR RI Ke Banten dalam rangka meninjau pemagaran laut di 6
+                    kecamatan di Kabupaten Tangerang</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td colspan="2">Alat angkutan yang diperlukan</td>
+                <td>Transportasi Umum</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td colspan="2">a. Tempat berangkat<br>b. Tempat tujuan</td>
+                <td>Jakarta<br>Tangerang</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td colspan="2">a. Lamanya perjalanan dinas<br>b. Tanggal berangkat<br>c. Tanggal harus kembali</td>
+                <td>1 (satu) hari<br>22 Januari 2025<br>22 Januari 2025</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Pengikut Nama<br>1<br>2<br>3</td>
+                <td>Umur</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td colspan="2">Pembebanan anggaran:<br>a. Instansi<br>b. Mata anggaran</td>
+                <td>LEMBAGA DPR RI<br>001030.CF.5803.ABC.005.054.A.524111</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td colspan="2">Keterangan lain-lain</td>
+                <td>ST No: ST.03/KOM IV/DPR RI/I/2025 Tanggal 20 Januari 2025</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="4" style="text-align: right;">
+                    <table style="width: 100%; border: none;">
+                        <tr>
+                            <td style="width: 50%; border: none;"></td>
+                            <td style="width: 50%; text-align: center; border: none;">
+                                <div style="text-align: left;">Dikeluarkan di: JAKARTA</div>
+                                <div style="text-align: left;">Pada tanggal: 20 Januari 2025</div>
+                                <br>
+                                <p>PEJABAT PEMBUAT KOMITMEN BIDANG PERSIDANGAN I</p>
+                                <br><br><br><br>
+                                <p><u><b>EFFIE, S.SOS., M.A.P.</b></u><br>NP. 19680215 199003 2 003</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 </body>
+
 </html>
