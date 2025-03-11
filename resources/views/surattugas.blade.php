7e4b3f6e0432b5cfef2027ef71476b7476971e5d
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2025-03-11 11:19:43 +0700

    Enhance Surat Tugas layout with new header, improved watermark positioning, and added page break for better document formatting

--- resources/views/surattugas.blade.php
+++ resources/views/surattugas.blade.php
@@ -3,19 +3,23 @@
 
 <head>
     <style>
-        body {
+        /* body {
             font-family: Arial, sans-serif;
-        }
+        } */
 
         #watermark {
             position: fixed;
-            top: 13%;
+            top: 25%;
             width: 100%;
             text-align: center;
             opacity: .1;
             /* transform: rotate(10deg); */
             transform-origin: 50% 50%;
             z-index: -1000;
+        }
+
+        .page_break {
+            page-break-before: always;
         }
     </style>
     <meta charset="UTF-8">
@@ -26,6 +30,155 @@
     <div id="watermark">
         <img src="{{ public_path('images/watermark-setjen-dpr-ri.png') }}" alt="Watermark"
             style="width: 500px; opacity: 0.6;">
+    </div>
+    <div>
+        <table style="width: 100%;">
+            <tr>
+                <td style="width: 15%;">
+                    <img src="{{ public_path('images/watermark-setjen-dpr-ri.png') }}" alt="Logo DPR RI" style="width: 80px;">
+                </td>
+                <td style="width: 85%; text-align: center;">
+                    <div style="font-family: Arial, sans-serif; font-weight: bold; font-size: 18px;">SEKRETARIAT JENDERAL <br> DEWAN
+                        PERWAKILAN RAKYAT REPUBLIK INDONESIA</div>
+                    <div style="font-family: Arial, sans-serif; font-size: 12px;">JLN. JENDERAL GATOT SUBROTO JAKARTA
+                        KODE POS 10270 <br> TELP (021) 5715 349 FAX (021) 5715 423 / 5715 925, WEBSITE: www.dpr.go.id
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <hr>
+    <div style="text-align: center; font-weight: bold;">
+        <br>
+        SURAT TUGAS
+        <br>
+        NOMOR: ......... (1)
+    </div>
+
+    <br>
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 30%; vertical-align: top; font-weight: bold;">Menimbang</td>
+            <td style="width: 5%; text-align: center; vertical-align: top;">:</td>
+            <td style="width: 65%; vertical-align: top;">Bahwa dengan meningkatnya peran dan fungsi DPR RI, maka untuk
+                mendukung kegiatan DPR RI tersebut khususnya dalam melaksanakan perjalana dinas diperlukan prose teknis
+                dan administrasi yang cepat dan tepat;</td>
+        </tr>
+        <tr>
+            <td style="width: 30%; vertical-align: top; font-weight: bold;">Dasar</td>
+            <td style="width: 5%; text-align: center; vertical-align: top;">:</td>
+            <td style="width: 65%; vertical-align: top;">
+                <ol style="padding-left: 15; margin: 0;">
+                    <li>Peraturan Menteri Keuangan Nomor 119/PMK.05/2023 tentang Perubahan atas Peraturan Menteri
+                        Keuangan Nomor 113/PMK.05/2012 tentang Perjalanan Dinas Bagi Pejabat Negara, Pegawai Negeri dan
+                        Pegawai Tidak Tetap;</li>
+                    <li>Peraturan Menteri Keuangan Nomor 49/PMK.02/2023 tentang Standar Biaya Masukan Tahun Anggaran
+                        2024;</li>
+                    <li>Peraturan Sekretaris Jenderal Dewan Perwakilan Rakyat Republik Indonesia Nomor 1 Tahun 2024
+                        tentang Perjalanan Dinas Sekretariat Jenderal;</li>
+                </ol>
+            </td>
+        </tr>
+    </table>
+
+    <br>
+    Atas persetujuan Pimpinan Sekretariat Jenderal DPR RI, maka dengan Indonesia
+
+    <div style="text-align: center;">
+        <br>
+        Memberi tugas
+    </div>
+
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 30%; vertical-align: top; font-weight: bold;">Kepada</td>
+            <td style="width: 5%; text-align: center; vertical-align: top;">:</td>
+            <td style="width: 65%; vertical-align: top;">Nama terlampir</td>
+        </tr>
+        <tr>
+            <td style="width: 30%; vertical-align: top; font-weight: bold;">Untuk</td>
+            <td style="width: 5%; text-align: center; vertical-align: top;">:</td>
+            <td style="width: 65%; vertical-align: top;">Melakukan perjalanan dinas ke ...(2) dalam rangka ...(3) selama
+                ...(4) hari terhitung mulai tanggal ...(5) sampai dengan tanggal ...(6). Seluruh biaya yang berkaitan
+                dengan penugasan tersebut dibebankan pada Mata Anggaran ...(7)</td>
+        </tr>
+    </table>
+
+    <br>
+    Demikian Surat Tugas ini agar dilaksanakan dengan sebaik-baiknya dan penuh tanggungjawab.
+
+    <br><br>
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 50%;"></td>
+            <td style="width: 50%; text-align: center;">
+                Jakarta, ...(8)
+                <br>
+                Sekretaris Jenderal DPR RI
+                <br><br><br><br>
+                <u>...(9)</u>
+                <br>
+                NIP. ...(10)
+            </td>
+        </tr>
+    </table>
+
+    <br>
+    <ol>
+        <li>Sekretaris Jenderal</li>
+        <li>Inspektur Utama</li>
+        <li>Kepala Biro Keuangan</li>
+        <li>Kepala Biro Sumber Daya Manusia Aparatur</li>
+    </ol>
+
+    <div class="page_break">
+        <table style="width: 100%;">
+            <tr>
+                <td style="width: 50%;"></td>
+                <td style="width: 50%;">
+                    Lampiran Surat Tugas
+                    <br>
+                    <table style="width: 100%;">
+                        <tr>
+                            <td style="width: 20%;">Nomor</td>
+                            <td style="width: 80%;">: ...(13)</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%;">Tanggal</td>
+                            <td style="width: 80%;">: ...(14)</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+        <br>
+        <div style="text-align: center; font-weight: bold;">
+            DAFTAR NAMA
+        </div>
+        <br>
+        <table border="1" cellspacing="0" cellpadding="5" style="width: 100%;">
+            <thead style="background-color: #f2f2f2;">
+                <tr>
+                    <th style="text-align: center;">NO</th>
+                    <th>NAMA</th>
+                    <th>NO. ANGGOTA / NIP</th>
+                    <th>JABATAN / GOLONGAN</th>
+                    <th>FRAKSI / BAGIAN</th>
+                </tr>
+            </thead>
+            <tbody>
+                @for ($i = 1; $i <= 7; $i++)
+                    <tr>
+                        <td style="text-align: center;">{{ $i }}</td>
+                        <td>Nama {{ $i }}</td>
+                        <td style="text-align: center;">123456{{ $i }}</td>
+                        <td>Jabatan {{ $i }}</td>
+                        <td>Fraksi {{ $i }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
     </div>
 </body>
 
